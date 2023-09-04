49e9f1a12e92701f8e4e4aaf53058962c8b0c834
DKrishna1307 <142470187+DKrishna1307@users.noreply.github.com>
2023-09-04 17:20:43 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -15,7 +15,10 @@ def gittest(repo,pipelinejob)
   git "https://github.com/DKrishna1307/${repo}.git"
   sh "java -jar /var/lib/jenkins/workspace/${pipelinejob}/testing.jar"
 }
-
+def gitdelivery(pipelinejob,IP,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${pipelinejob}/webapp/target/webapp.war ubuntu@${IP}:/var/lib/tomcat9/webapps/${appname}.war"
+}
 
 
   
